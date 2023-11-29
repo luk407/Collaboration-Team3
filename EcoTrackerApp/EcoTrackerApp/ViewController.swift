b9b2a713fc4c5681aa3792613ae9921b6d009279
luk407 <luka.gazdeliani07@gmail.com>
2023-11-29 13:13:18 +0400

    create project

---
+++ EcoTrackerApp/EcoTrackerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EcoTrackerApp
+//
+//  Created by Luka Gazdeliani on 29.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
