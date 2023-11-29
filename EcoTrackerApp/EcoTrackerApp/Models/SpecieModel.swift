3876711cc00447eb1ddd4318e1ee6d016efedbdd
ekakele <ekaakelenjeridze@gmail.com>
2023-11-29 22:59:13 +0400

    feat: add specie model

--- EcoTrackerApp/EcoTrackerApp/Models/SpecieModel.swift
+++ EcoTrackerApp/EcoTrackerApp/Models/SpecieModel.swift
@@ -3,6 +3,47 @@
 //  EcoTrackerApp
 //
 //  Created by Luka Gazdeliani on 29.11.23.
+//  Modified by Eka Kelenjeridze on 29.11.23.
 //
 
 import Foundation
+
+// MARK: - CityID Model
+struct CityID: Decodable {
+    let results: [Result]
+    
+    struct Result: Codable {
+        let id: Int
+    }
+}
+
+// MARK: - Species Model
+struct Species: Decodable {
+    let results: [Result]
+}
+
+struct Result: Decodable {
+    let taxon: Taxon
+}
+
+struct Taxon: Decodable {
+    let name: String
+    let defaultPhoto: DefaultPhoto
+    let wikipediaURL: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case defaultPhoto = "default_photo"
+        case wikipediaURL = "wikipedia_url"
+    }
+}
+
+struct DefaultPhoto: Decodable {
+    let mediumURL: String
+    let attribution: String
+    
+    enum CodingKeys: String, CodingKey {
+        case mediumURL = "medium_url"
+        case attribution
+    }
+}
