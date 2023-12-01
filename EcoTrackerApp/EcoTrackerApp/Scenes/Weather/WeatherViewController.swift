ef839400c1e62dd504e2f1b32b55c0a7ba7189c0
luk407 <luka.gazdeliani07@gmail.com>
2023-12-01 20:21:34 +0400

    add elements to view controller

--- EcoTrackerApp/EcoTrackerApp/Scenes/Weather/WeatherViewController.swift
+++ EcoTrackerApp/EcoTrackerApp/Scenes/Weather/WeatherViewController.swift
@@ -8,11 +8,201 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+final class WeatherViewController: UIViewController {
+    
+    //MARK: - Properties
+    private let mainStackView = UIStackView()
+    
+    private let latTextField = UITextField()
+    
+    private let longTextField = UITextField()
+    
+    private let showWeatherButton = UIButton()
+    
+    private let fetchedFactsStackView = UIStackView()
+    
+    private var countryLabel = UILabel()
+    
+    private var cityLabel = UILabel()
+    
+    private var weatherMainLabel = UILabel()
+    
+    private var weatherDescriptionLabel = UILabel()
 
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
     }
 
+    //MARK: Setup Methods
+    private func addSubViews() {
+        view.addSubview(mainStackView)
+        mainStackView.addArrangedSubview(latTextField)
+        mainStackView.addArrangedSubview(longTextField)
+        mainStackView.addArrangedSubview(showWeatherButton)
+        mainStackView.addArrangedSubview(fetchedFactsStackView)
+        fetchedFactsStackView.addArrangedSubview(countryLabel)
+        fetchedFactsStackView.addArrangedSubview(cityLabel)
+        fetchedFactsStackView.addArrangedSubview(weatherMainLabel)
+        fetchedFactsStackView.addArrangedSubview(weatherDescriptionLabel)
+    }
+    
+    private func setupConstraints() {
+        setupMainStackViewConstraints()
+        setupLatTextFieldConstraints()
+        setupLongTextFieldConstraints()
+        setupShowWeatherButtonConstraints()
+        setupFetchedFactsStackViewConstraints()
+        setupCountryLabelConstraints()
+        setupCityLabelConstraints()
+        setupWeatherMainLabelConstraints()
+        setupWeatherDescriptionLabelConstraints()
+    }
+    
+    private func setupUI() {
+        setupMainStackViewUI()
+        setupLatTextFieldUI()
+        setupLongTextFieldUI()
+        setupShowWeatherButtonUI()
+        setupFetchedFactsStackViewUI()
+        setupCountryLabelUI()
+        setupCityLabelUI()
+        setupWeatherMainLabelUI()
+        setupWeatherDescriptionLabelUI()
+    }
+    
+    //MARK: - Setup Constraints
+    private func setupMainStackViewConstraints() {
+        NSLayoutConstraint.activate([
+            mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            mainStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -250),
+            mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+        ])
+    }
+    
+    private func setupLatTextFieldConstraints() {
+        NSLayoutConstraint.activate([
+            latTextField.leadingAnchor.constraint(equalTo: mainStackView.leadingAnchor),
+            latTextField.trailingAnchor.constraint(equalTo: mainStackView.trailingAnchor),
+            latTextField.heightAnchor.constraint(equalToConstant: 30)
+        ])
+    }
+    
+    private func setupLongTextFieldConstraints() {
+        NSLayoutConstraint.activate([
+            longTextField.leadingAnchor.constraint(equalTo: mainStackView.leadingAnchor),
+            longTextField.trailingAnchor.constraint(equalTo: mainStackView.trailingAnchor),
+            longTextField.heightAnchor.constraint(equalToConstant: 30)
+        ])
+    }
+    
+    private func setupShowWeatherButtonConstraints() {
+        NSLayoutConstraint.activate([
+            showWeatherButton.leadingAnchor.constraint(equalTo: mainStackView.leadingAnchor),
+            showWeatherButton.trailingAnchor.constraint(equalTo: mainStackView.trailingAnchor),
+            showWeatherButton.widthAnchor.constraint(equalToConstant: 100),
+            showWeatherButton.heightAnchor.constraint(equalToConstant: 30)
+        ])
+    }
+    
+    private func setupFetchedFactsStackViewConstraints() {
+        NSLayoutConstraint.activate([
+            fetchedFactsStackView.heightAnchor.constraint(equalToConstant: 250),
+            fetchedFactsStackView.leadingAnchor.constraint(equalTo: mainStackView.leadingAnchor),
+            fetchedFactsStackView.trailingAnchor.constraint(equalTo: mainStackView.trailingAnchor)
+        ])
+    }
+    
+    private func setupCountryLabelConstraints() {
+        NSLayoutConstraint.activate([
+            countryLabel.leadingAnchor.constraint(equalTo: fetchedFactsStackView.leadingAnchor, constant: 10),
+            countryLabel.trailingAnchor.constraint(equalTo: fetchedFactsStackView.trailingAnchor, constant: -10),
+        ])
+    }
+    
+    private func setupCityLabelConstraints() {
+        NSLayoutConstraint.activate([
+            cityLabel.leadingAnchor.constraint(equalTo: fetchedFactsStackView.leadingAnchor, constant: 10),
+            cityLabel.trailingAnchor.constraint(equalTo: fetchedFactsStackView.trailingAnchor, constant: -10),
+        ])
+    }
+    
+    private func setupWeatherMainLabelConstraints() {
+        NSLayoutConstraint.activate([
+            weatherMainLabel.leadingAnchor.constraint(equalTo: fetchedFactsStackView.leadingAnchor, constant: 10),
+            weatherMainLabel.trailingAnchor.constraint(equalTo: fetchedFactsStackView.trailingAnchor, constant: -10),
+        ])
+    }
+    
+    private func setupWeatherDescriptionLabelConstraints() {
+        NSLayoutConstraint.activate([
+            weatherDescriptionLabel.leadingAnchor.constraint(equalTo: fetchedFactsStackView.leadingAnchor, constant: 10),
+            weatherDescriptionLabel.trailingAnchor.constraint(equalTo: fetchedFactsStackView.trailingAnchor, constant: -10),
+        ])
+    }
+    
+    //MARK: - Private Methods
+    private func setupMainStackViewUI() {
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        mainStackView.axis = .vertical
+        mainStackView.spacing = 20
+        mainStackView.alignment = .center
+        mainStackView.distribution = .fillProportionally
+    }
+    
+    private func setupLatTextFieldUI() {
+        latTextField.textAlignment = .left
+        latTextField.placeholder = "Type Latitude"
+        latTextField.borderStyle = .roundedRect
+    }
+    
+    private func setupLongTextFieldUI() {
+        longTextField.textAlignment = .left
+        longTextField.placeholder = "Type Longitude"
+        longTextField.borderStyle = .roundedRect
+    }
+    
+    private func setupShowWeatherButtonUI() {
+        showWeatherButton.setTitleColor(.black, for: .normal)
+        showWeatherButton.setTitle("Show Weather", for: .normal)
+        showWeatherButton.layer.cornerRadius = 8
+        showWeatherButton.layer.backgroundColor = UIColor(red: 125/255, green: 125/255, blue: 125/255, alpha: 0.3).cgColor
+    }
+    
+    private func setupFetchedFactsStackViewUI() {
+        fetchedFactsStackView.axis = .vertical
+        fetchedFactsStackView.spacing = 10
+        fetchedFactsStackView.alignment = .center
+        fetchedFactsStackView.distribution = .fillProportionally
+        fetchedFactsStackView.layer.backgroundColor = UIColor(red: 125/255, green: 125/255, blue: 125/255, alpha: 0.3).cgColor
+        fetchedFactsStackView.layer.cornerRadius = 8
+    }
+    
+    private func setupCountryLabelUI() {
+        countryLabel.font = UIFont.boldSystemFont(ofSize: 25)
+        countryLabel.textAlignment = .left
+        countryLabel.text = "Country"
+    }
+    
+    private func setupCityLabelUI() {
+        cityLabel.font = UIFont.boldSystemFont(ofSize: 25)
+        cityLabel.textAlignment = .left
+        cityLabel.text = "City"
+    }
+    
+    private func setupWeatherMainLabelUI() {
+        weatherMainLabel.font = UIFont.boldSystemFont(ofSize: 25)
+        weatherMainLabel.textAlignment = .left
+        weatherMainLabel.text = "Weather"
+        weatherMainLabel.numberOfLines = 0
+    }
+    
+    private func setupWeatherDescriptionLabelUI() {
+        weatherDescriptionLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        weatherDescriptionLabel.textAlignment = .left
+        weatherDescriptionLabel.text = "Description"
+        weatherMainLabel.numberOfLines = 0
+    }
 }
